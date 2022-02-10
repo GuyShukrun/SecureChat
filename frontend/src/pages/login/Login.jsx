8d1e0e188591ae5bc46b0a4e99537adf3017f2fa
GuyShukrun <63101109+GuyShukrun@users.noreply.github.com>
2022-02-10 19:04:46 +0200

    Changed vh on login screen

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -26,65 +26,60 @@ export default function Login({ user, setUser }) {
   };
 
   return (
-    <div className="vh-100">
-      <div className="container h-100 w-100 d-flex justify-content-center align-items-center">
-        <div className="row ">
-          <div className="col ">
-            <h1 className="logo text-center mb-5 mt-5">SecureChat</h1>
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-              className="img-fluid"
-              alt=""
-            />
-          </div>
-          <div className="col ">
-            <h1 className="text-center loginLogo">Login</h1>
-            <form onSubmit={handleLogin}>
-              <div className="form-group mt-5">
-                <label className="mb-1">Email address</label>
-                <input
-                  type="email"
-                  className="form-control "
-                  ref={email}
-                  aria-describedby="emailHelp"
-                  placeholder="Enter email"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  We'll never share your email with anyone else.
-                </small>
-              </div>
-              <div className="form-group mt-2">
-                <label className="mb-1">Password</label>
-                <input
-                  type="password"
-                  ref={password}
-                  className="form-control"
-                  placeholder="Password"
-                />
-              </div>
-              {!loading ? (
-                <button type="submit" className="btn btn-primary mt-4">
-                  Login
-                </button>
-              ) : (
-                <div
-                  class="spinner-border text-primary mt-4"
-                  role="status"
-                ></div>
-              )}
-              <p className="notRegisteredYet mt-5">
-                Not registered yet?
-                <a
-                  className="text-decoration-none"
-                  style={{ color: "#6441a5" }}
-                  href="http://localhost:3000/register"
-                >
-                  {" "}
-                  Create an Account
-                </a>
-              </p>
-            </form>
-          </div>
+    <div className="container loginContainer d-flex justify-content-center align-items-center">
+      <div className="row ">
+        <div className="col ">
+          <h1 className="logo text-center mb-5 mt-5">SecureChat</h1>
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+            className="img-fluid"
+            alt=""
+          />
+        </div>
+        <div className="col ">
+          <h1 className="text-center loginLogo">Login</h1>
+          <form onSubmit={handleLogin}>
+            <div className="form-group mt-5">
+              <label className="mb-1">Email address</label>
+              <input
+                type="email"
+                className="form-control "
+                ref={email}
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div className="form-group mt-2">
+              <label className="mb-1">Password</label>
+              <input
+                type="password"
+                ref={password}
+                className="form-control"
+                placeholder="Password"
+              />
+            </div>
+            {!loading ? (
+              <button type="submit" className="btn btn-primary mt-4">
+                Login
+              </button>
+            ) : (
+              <div class="spinner-border text-primary mt-4" role="status"></div>
+            )}
+            <p className="notRegisteredYet mt-5">
+              Not registered yet?
+              <a
+                className="text-decoration-none"
+                style={{ color: "#6441a5" }}
+                href="http://localhost:3000/register"
+              >
+                {" "}
+                Create an Account
+              </a>
+            </p>
+          </form>
         </div>
       </div>
     </div>
